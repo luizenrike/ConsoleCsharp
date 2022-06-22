64170334b4bece2f2cf89ad97f7bcd1451be5c7d
luizenrik <luizhenrique.99@outlook.com>
2022-06-22 17:50:08 -0300

    Programa para acessar APIs e receber os resultados das consultas

---
+++ ConsultaAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsultaAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            consultarAPI();
+        }
+
+        private static void consultarAPI()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://randomuser.me/api"); // endereço que será consultado
+            var message = new HttpResponseMessage(); // resposta da consulta
+            message = client.GetAsync("/?nat=us&randomapi").GetAwaiter().GetResult();
+            string result = message.Content.ReadAsStringAsync().Result; //resultado gerado pela consulta
+            // nesse caso, essa consulta retorna informações em json.
+            Console.WriteLine(result);
+        }
+    }
+}
