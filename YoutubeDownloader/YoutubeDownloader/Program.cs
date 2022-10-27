d87d36e3deac3e8e2ca0773b2b6843acafaa82d0
Luiz Henrique <luizhenrique.99@outlook.com>
2022-10-27 11:32:41 -0300

    Aplicação para download de videos do youtube usando a biblioteca VideoLibrary.

---
+++ YoutubeDownloader/YoutubeDownloader/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using VideoLibrary;
+
+VideoDownload();
+
+// codificação básica para testagem 
+static void VideoDownload()
+{
+    Console.Write("URL: ");
+    string URI = Console.ReadLine();
+
+    Console.Write("QUALITY(360, 480, 720): ");
+    int quality = Convert.ToInt32(Console.ReadLine());
+
+    try
+    {
+        var youtube = YouTube.Default;
+        
+        var videoInfos = youtube.GetAllVideosAsync(URI).GetAwaiter().GetResult();
+        //var video = youtube.GetVideoAsync(URI).GetAwaiter().GetResult();
+        Thread.Sleep(20);
+        
+        Video video = videoInfos.First(v => v.Resolution == quality);
+        Console.WriteLine(video.FullName+" downloaded");
+
+        if (!Directory.Exists(@"C:\youtube"))
+        {
+            Directory.CreateDirectory(@"C:\youtube");
+        }
+        File.WriteAllBytes(@"C:\youtube\" + video.FullName, video.GetBytesAsync().GetAwaiter().GetResult());
+        Console.WriteLine("Download completed, video in directory: " + @"C:\youtube");
+    }catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+}
